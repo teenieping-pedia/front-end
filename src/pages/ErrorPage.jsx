d7e4a8110bcb7516fc4b0a6f74a8d1ac811dc747
gangmin2 <min0115b@gmail.com>
2024-10-13 00:17:24 +0900

    feat: 에러 페이지 구현

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,21 @@
+import { useRouteError } from "react-router-dom";
+import "../styles/error.css";
+
 const ErrorPage = () => {
+  const error = useRouteError();
+  console.error(error);
+
   return (
-    <div>
-      Error Page
+    <div className="error-container">
+      <div>
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ErrorPage;
