8df21f4e6592b305aca672ddf96d1a6d9408e9be
gangmin2 <min0115b@gmail.com>
2024-10-12 22:59:48 +0900

    feat: 티니핑 상세 페이지 구현

--- src/components/TeeniepingList.jsx
+++ src/components/TeeniepingList.jsx
@@ -5,7 +5,7 @@ const TeeniepingList = ({ teeniepings }) => {
   return (
     <section className="list-container">
       {teeniepings && teeniepings.map((t) => (
-        <TeeniepingCard key={t.id} id={t.id} imageUrl={t.imageUrl} series={t.series} rank={t.rank} name={t.name} />
+        <TeeniepingCard key={t._id} id={t._id} imageUrl={t.imageUrl} series={t.series} rank={t.rank} name={t.name} />
       ))}
       {!teeniepings &&
         <p>데이터가 존재하지 않습니다.</p>
--- src/pages/TeeniepingDetail.jsx
+++ src/pages/TeeniepingDetail.jsx
@@ -1,7 +1,58 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Badge from "../components/Badge";
+import "../styles/detail.css";
+
 const TeeniepingDetail = () => {
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [teenieping, setTeenieping] = useState(null);
+
+  useEffect(() => {
+    const fetchTeenieping = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/teeniepings/${id}`);
+        const data = await response.json();
+        setTeenieping(data);
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchTeenieping();
+  }, [id]);
+
+  if (isLoading) return <p>Loading...</p>
+  if (teenieping === null) return <p>유효하지 않은 접근입니다.</p>
   return (
-    <div>
-      Teenieping Detail
+    <div className="detail-container">
+      <div className="image-container half-container">
+        <img className="image" src={teenieping.imageUrl} />
+      </div>
+      <div className="info-container half-container">
+        <div>
+          <Badge text={teenieping.series} />
+          <Badge text={teenieping.rank} />
+        </div>
+        <div>
+          <p className="name">{teenieping.name}</p>
+          <p className="description">{teenieping.description}</p>
+        </div>
+        <div>
+          <span className="col">소품</span>
+          <span className="row">{teenieping.items}</span>
+        </div>
+        <div>
+          <span className="col">마법</span>
+          <span className="row">{teenieping.magic}</span>
+        </div>
+        <div>
+          <span className="col">성격</span>
+          <span className="row">{teenieping.personality}</span>
+        </div>
+      </div>
     </div>
   )
 }
